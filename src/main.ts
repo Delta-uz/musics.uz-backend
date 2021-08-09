579630a4b963fe2ae281118c56311342a8224bc1
mcpeblocker <mcpeblocker@gmail.com>
2021-08-09 16:50:02 +0500

    disable swagger

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
 
-  const options = new DocumentBuilder()
-    .setTitle('Musics API')
-    .setDescription('Musics platform API')
-    .setVersion('1.0')
-    .addTag('musics')
-    .addCookieAuth('Authorization')
-    .build()
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  // const options = new DocumentBuilder()
+  //   .setTitle('Musics API')
+  //   .setDescription('Musics platform API')
+  //   .setVersion('1.0')
+  //   .addTag('musics')
+  //   .addCookieAuth('Authorization')
+  //   .build()
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
